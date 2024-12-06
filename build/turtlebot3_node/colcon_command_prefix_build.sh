a2974cc42faff19b46da99fc3dc4e49a8f8bb5d4
Behrouz Ghamkhar <behroozghamkhar@gmail.com>
2024-12-06 16:42:09 +0100

    Remove build, install and log directories from the ws.

--- build/turtlebot3_node/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/behrouz/ws24-multi-robot-task-distribution/install/dynamixel_sdk/share/dynamixel_sdk/package.sh"
-. "/home/behrouz/ws24-multi-robot-task-distribution/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
